73c482d510431de0d7c69426584b659d7b254c68
ItzRig3l <itzrigel113@gmail.com>
2022-03-04 09:34:48 +0700

    u

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            var intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+        findViewById<Button>(R.id.button2).setOnClickListener {
+            var intent2 = Intent(this, MainActivity3::class.java)
+            startActivity(intent2)
+        }
+        findViewById<Button>(R.id.button3).setOnClickListener {
+            var intent3 = Intent(this, MainActivity4::class.java)
+            startActivity(intent3)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        findViewById<Button>(R.id.imageButton2).setOnClickListener {
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
